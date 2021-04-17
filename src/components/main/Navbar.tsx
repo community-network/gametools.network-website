3a640c69681343d5c555d41dbd0592861681ffa3
Zefanja Jobse <zefanjaj@gmail.com>
2021-04-17 10:00:39 +0000

    get old stuff back

--- src/components/main/Navbar.tsx
+++ src/components/main/Navbar.tsx
@@ -1,6 +1,9 @@
 import { HashRouter, Switch, Route, Link } from "react-router-dom";
+import '../../locales/config';
+import { useTranslation } from 'react-i18next';
 
 export function Navbar() {
+    const { t } = useTranslation();
     return (
         <Link to="/stats/pc/iiTzArcur/bf1">test</Link>
     )
--- src/components/stats/main.tsx
+++ src/components/stats/main.tsx
@@ -1,12 +1,11 @@
-import {
-    BrowserRouter as Router,
-    Route,
-    Link,
-    RouteComponentProps
-  } from "react-router-dom";
+import { RouteComponentProps } from "react-router-dom";
+import '../../locales/config';
+import { useTranslation } from 'react-i18next';
+
 type TParams = { plat: string, eaid: string, game: string }
 
 function Stats({ match }: RouteComponentProps<TParams>) {
+    const { t } = useTranslation();
     return (
     <div>
         {match.params.plat}
