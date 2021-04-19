f92fdc607b29a8b38a16e526222684b2ee0ac036
Zefanja Jobse <zefanjaj@gmail.com>
2021-04-19 18:42:15 +0000

    games per platform list in static.tsx

--- src/api/static.tsx
+++ src/api/static.tsx
@@ -1,1 +1,7 @@
-const withoutPlatform = ["bf1", "bfv", "bf2"]
+export const platformGames = {
+    "pc": ["bf2", "bf3", "bf4", "bfh", "bf1", "bfv"],
+    "xboxone": ["bf4", "bfh"],
+    "xbox360": ["bf3", "bf4", "bfh"],
+    "ps4": ["bf4", "bfh"],
+    "ps3": ["bf3", "bf4", "bfh"]
+}
