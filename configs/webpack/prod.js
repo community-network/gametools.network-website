ecd480420d647b0a8a6b4349614041005e78a6d5
Zefanja Jobse <zefanjaj@gmail.com>
2021-04-21 19:38:44 +0000

    fix prod

--- configs/webpack/prod.js
+++ configs/webpack/prod.js
@@ -8,7 +8,7 @@ module.exports = merge(commonConfig, {
   mode: "production",
   entry: "./index.tsx",
   output: {
-    filename: "js/bundle.[contenthash].min.js",
+    filename: "js/bundle.gametools-prod-edge.min.js",
     path: resolve(__dirname, "../../dist"),
     publicPath: "/",
   },
