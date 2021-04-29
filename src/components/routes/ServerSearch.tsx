a1a2571c135ee96fc23694305c52f4bc5b4d479e
Zefanja Jobse <zefanjaj@gmail.com>
2021-04-29 18:58:33 +0000

    fix enter press serversearch

--- src/components/routes/ServerSearch.tsx
+++ src/components/routes/ServerSearch.tsx
@@ -114,25 +114,23 @@ function Search() {
             <AltDescription>{t("serverSearch.description")}</AltDescription>
         </Align>
         <Align>
-            <form>
-                <SearchBox placeholder={t("serverSearch.searchPlaceholder")} value={searchTerm} onChange={(ev: React.ChangeEvent<HTMLInputElement>):
-                    void => setSearchTerm(ev.target.value)}/>
-                <BigSelectSecondary value={gameName} onChange={(ev: React.ChangeEvent<HTMLSelectElement>):
-                        void => setGameName(ev.target.value)}>
-                    <option value="bf1">Battlefield 1</option>
-                    <option value="bf2.bf2hub">Battlefield 2 (bf2hub)</option>
-                    <option value="bf2.playbf2">Battlefield 2 (playbf2)</option>
-                    <option value="bfbc2">Battlefield BC 2</option>
-                    <option value="bf2142">Battlefield 2142 (.com)</option>
-                    <option value="bf1942">Battlefield 1942</option>
-                    <option value="bfvietnam">Battlefield Vietnam</option>
-                    <option value="bf3">Battlefield 3</option>
-                    <option value="bf4">Battlefield 4</option>
-                    <option value="bfh">Battlefield Hardline</option>
-                    <option value="bfv">Battlefield 5</option>
-                </BigSelectSecondary>
-                {/* <BigButtonSecondary type="submit">{t("serverSearch.search")} <RightArrow/></BigButtonSecondary> */}
-            </form>
+            <SearchBox placeholder={t("serverSearch.searchPlaceholder")} value={searchTerm} onChange={(ev: React.ChangeEvent<HTMLInputElement>):
+                void => setSearchTerm(ev.target.value)}/>
+            <BigSelectSecondary value={gameName} onChange={(ev: React.ChangeEvent<HTMLSelectElement>):
+                    void => setGameName(ev.target.value)}>
+                <option value="bf1">Battlefield 1</option>
+                <option value="bf2.bf2hub">Battlefield 2 (bf2hub)</option>
+                <option value="bf2.playbf2">Battlefield 2 (playbf2)</option>
+                <option value="bfbc2">Battlefield BC 2</option>
+                <option value="bf2142">Battlefield 2142 (.com)</option>
+                <option value="bf1942">Battlefield 1942</option>
+                <option value="bfvietnam">Battlefield Vietnam</option>
+                <option value="bf3">Battlefield 3</option>
+                <option value="bf4">Battlefield 4</option>
+                <option value="bfh">Battlefield Hardline</option>
+                <option value="bfv">Battlefield 5</option>
+            </BigSelectSecondary>
+            {/* <BigButtonSecondary type="submit">{t("serverSearch.search")} <RightArrow/></BigButtonSecondary> */}
         </Align>
         <Title>{t("serverSearch.results")}</Title>
         <Results game={gameName} loading={loading} stats={stats} error={error}/>
