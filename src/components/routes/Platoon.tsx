65d83ac0dfb8cc56fea2726ab20660a271992419
Zefanja Jobse <zefanjaj@gmail.com>
2021-07-12 23:30:06 +0200

    better platoon view

--- src/components/routes/Platoon.tsx
+++ src/components/routes/Platoon.tsx
@@ -4,7 +4,7 @@ import { RouteComponentProps } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import styled from "styled-components";
 import "../../assets/scss/App.scss";
-import { GetStats, PlatoonStats, PlatoonPlayer } from "../../api/GetStats";
+import { GetStats, PlatoonStats } from "../../api/GetStats";
 import { useQuery } from "react-query";
 import {
   AltText,
@@ -14,8 +14,18 @@ import {
   Align,
   AlignW,
   Box,
+  Column,
+  Row,
+  SmallSearchBox,
+  SelectPrimary,
+  ButtonLink,
 } from "../Materials";
 import { getLanguage } from "../../locales/config";
+
+const Spacing = styled.div`
+  margin-top: 2rem;
+  margin-bottom: 2rem;
+`;
 
 const Description = styled.p`
   ${AltText}
@@ -30,7 +40,8 @@ const DescriptionPart = styled.p`
 `;
 
 const Title = styled.h2`
-  margin-top: 2rem;
+  margin: 0 33px;
+  padding-bottom: 1rem;
 `;
 
 const PlatoonTitle = styled.h2`
@@ -54,15 +65,10 @@ const PlatoonImage = styled.div<IPlatoonImage>`
   border-radius: 10px;
 `;
 
-const MapImage = styled.img`
-  margin-top: 12px;
-  max-height: 4rem;
-  margin-right: 0.7rem;
-  border-radius: 2px;
-`;
-
-const PlatoonInfo = styled.div`
-  margin-top: 6px;
+const MemberImage = styled.img`
+  max-width: 8rem;
+  max-height: 3rem;
+  margin-right: 1.5rem;
 `;
 
 interface Views {
@@ -71,19 +77,32 @@ interface Views {
   platoon: PlatoonStats;
 }
 
+function dynamicSort(property: string) {
+  let sortOrder = 1;
+  if (property[0] === "-") {
+    sortOrder = -1;
+    property = property.substr(1);
+  }
+  return function (a: {}, b: {}) {
+    const result =
+      a[property] < b[property] ? -1 : a[property] > b[property] ? 1 : 0;
+    return result * sortOrder;
+  };
+}
+
 function Results(props: Views): React.ReactElement {
   const { t } = useTranslation();
   const platoon = props.platoon;
+
+  let members = [];
+  const [searchTerm, setSearchTerm] = React.useState<string>("");
+  const [sortType, setSortType] = React.useState<string>("-kills");
+
   if (!props.loading && !props.error) {
-    const platoonMembers = {
-      General: [],
-      Colonel: [],
-      Lieutenant: [],
-      Private: [],
-    };
-    platoon.members.map((key: PlatoonPlayer) => {
-      platoonMembers[key.role].push(key);
+    members = platoon.members.filter((item: { name: string; role: any }) => {
+      return item.name.toLowerCase().includes(searchTerm.toLowerCase());
     });
+    members = members.sort(dynamicSort(sortType));
     return (
       <div>
         <AlignW>
@@ -108,44 +127,75 @@ function Results(props: Views): React.ReactElement {
             )}
           </div>
         </AlignW>
-        {Object.entries(platoonMembers).map((rank: unknown, index: number) => {
-          return (
-            <div key={index}>
-              <Title>{t(`platoon.members.${rank[0]}`)}</Title>
-              <Align>
-                {platoonMembers[rank[0]].length !== 0 ? (
-                  platoonMembers[rank[0]].map(
-                    (member: PlatoonPlayer, index: number) => {
-                      return (
-                        <Box key={index}>
-                          <AlignW>
-                            <div>
-                              <MapImage src={member.avatar} />
-                            </div>
-                            <PlatoonInfo>
-                              <h3>{member.name}</h3>
-                              <a
-                                href={`https://gametools.network/stats/pc/playerid/${member.id}?game=bf1`}
-                                target="_blank"
-                                rel="noreferrer"
-                              >
-                                {t("stats.view")}
-                              </a>
-                            </PlatoonInfo>
-                          </AlignW>
-                        </Box>
-                      );
-                    },
-                  )
-                ) : (
-                  <Box>
-                    <p>{t("platoon.members.empty")}</p>
-                  </Box>
-                )}
-              </Align>
-            </div>
-          );
-        })}
+        <Spacing>
+          <Align>
+            <Title>{t("platoon.members.main")}</Title>
+            <AlignW>
+              <SmallSearchBox
+                placeholder={t("platoon.searchPlayer")}
+                value={searchTerm}
+                onChange={(ev: React.ChangeEvent<HTMLInputElement>): void =>
+                  setSearchTerm(ev.target.value)
+                }
+              />
+              <SelectPrimary
+                value={sortType}
+                onChange={(ev: React.ChangeEvent<HTMLSelectElement>): void =>
+                  setSortType(ev.target.value)
+                }
+              >
+                <option value="default">{t("platoon.rows.role")}</option>
+                <option value="name">{t("platoon.rows.name")}</option>
+              </SelectPrimary>
+            </AlignW>
+          </Align>
+          {platoon.members !== [] ? (
+            <Box>
+              <div>
+                {members.map((key: any, index: number) => {
+                  return (
+                    <div key={index} style={{ margin: "0.8rem 0.2rem" }}>
+                      <Column style={{ marginTop: 0 }}>
+                        <Row>
+                          <Align>
+                            <MemberImage src={key.avatar} />
+                            <h4 style={{ marginTop: "2px", marginBottom: 0 }}>
+                              {key.name}
+                            </h4>
+                          </Align>
+                        </Row>
+                        <Row>
+                          <h4>{key.role}</h4>
+                        </Row>
+                        <Row>
+                          <ButtonLink
+                            style={{ marginTop: ".3rem" }}
+                            href={`https://gametools.network/stats/pc/playerid/${key.id}?game=bf1`}
+                            target="_blank"
+                            rel="noreferrer"
+                          >
+                            {t("stats.view")}
+                          </ButtonLink>
+                        </Row>
+                      </Column>
+                      <hr
+                        style={{
+                          marginTop: "0.6rem",
+                          width: "98%",
+                          border: "1px solid #282a3a",
+                        }}
+                      />
+                    </div>
+                  );
+                })}
+              </div>
+            </Box>
+          ) : (
+            <Box>
+              <p>{t("loading")}</p>
+            </Box>
+          )}
+        </Spacing>
       </div>
     );
   } else {
