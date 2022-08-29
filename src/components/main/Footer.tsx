782d1d2e9db02e6933e51bdd7761dbb9af19368e
Zefanja Jobse <zefanjaj@gmail.com>
2022-08-29 20:16:55 +0200

    .

--- src/components/main/Footer.tsx
+++ src/components/main/Footer.tsx
@@ -65,7 +65,6 @@ export function Footer(): JSX.Element {
     "https://github.com/Community-network",
     "https://api.gametools.network/",
     "https://discord.gametools.network/",
-    "/playgrounds/",
     "/privacy/",
     "https://top.gg/bot/714524944783900794",
   ];
