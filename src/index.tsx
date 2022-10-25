3a39789f187ad403bdbb23a02c0d5af45b03ef7e
Zefanja Jobse <zefanjaj@gmail.com>
2022-10-25 21:18:10 +0200

    add serviceworker

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,13 @@
 import * as React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./components/App";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 const container = document.getElementById("root");
 const root = createRoot(container!);
 root.render(<App />);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
