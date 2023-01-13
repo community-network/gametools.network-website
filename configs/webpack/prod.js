e510d485ccc77f4e2fb2a562c7d4ff7fd53c7fc8
Zefanja Jobse <zefanjaj@gmail.com>
2023-01-13 13:30:00 +0100

    different lazy load test

--- configs/webpack/prod.js
+++ configs/webpack/prod.js
@@ -1,29 +1,19 @@
 // production config
-const CopyPlugin = require("copy-webpack-plugin");
 const { merge } = require("webpack-merge");
 const { resolve } = require("path");
-const WorkboxPlugin = require("workbox-webpack-plugin");
 
 const commonConfig = require("./common");
 
 module.exports = merge(commonConfig, {
   mode: "production",
-  entry: "./index.tsx",
   output: {
-    filename: "js/bundle.gametools-prod-edge.min.js",
+    filename: "js/[name].[contenthash].min.js",
     path: resolve(__dirname, "../../dist"),
     publicPath: "/",
   },
   devtool: "source-map",
-  plugins: [
-    new CopyPlugin({
-      patterns: [{ from: "public", to: resolve(__dirname, "../../dist") }],
-    }),
-    new WorkboxPlugin.GenerateSW({
-      // these options encourage the ServiceWorkers to get in there fast
-      // and not allow any straggling "old" SWs to hang around
-      clientsClaim: true,
-      skipWaiting: true,
-    }),
-  ],
+  externals: {
+    react: "React",
+    "react-dom": "ReactDOM",
+  },
 });
