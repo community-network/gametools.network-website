20194a5d0d73b171e056966cb682a5505d7239a2
Zefanja Jobse <zefanjaj@gmail.com>
2023-09-01 13:38:28 +0200

    disallow sideeffects

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,13 +3,17 @@ import { BrowserRouter } from "react-router-dom";
 import "./../assets/scss/App.scss";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Views from "./Views";
+import { I18nextProvider } from "react-i18next";
+import i18n from "../locales/config";
 
 const queryClient = new QueryClient();
 
 const App = () => (
   <BrowserRouter>
     <QueryClientProvider client={queryClient}>
-      <Views />
+      <I18nextProvider i18n={i18n}>
+        <Views />
+      </I18nextProvider>
     </QueryClientProvider>
   </BrowserRouter>
 );
--- src/locales/config.ts
+++ src/locales/config.ts
@@ -79,3 +79,5 @@ export const getLanguage = (): string => {
   }
   return language;
 };
+
+export default i18n;
