e41e40630a1137ac725a65764130019d265e8b11
Zefanja Jobse <zefanjaj@gmail.com>
2023-09-01 20:23:12 +0200

    try without suspense

--- src/index.tsx
+++ src/index.tsx
@@ -6,15 +6,15 @@ const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(<App />);
 
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker
-      .register("/service-worker.js")
-      .then((registration) => {
-        console.log("SW registered: ", registration);
-      })
-      .catch((registrationError) => {
-        console.log("SW registration failed: ", registrationError);
-      });
-  });
-}
+// if ("serviceWorker" in navigator) {
+//   window.addEventListener("load", () => {
+//     navigator.serviceWorker
+//       .register("/service-worker.js")
+//       .then((registration) => {
+//         console.log("SW registered: ", registration);
+//       })
+//       .catch((registrationError) => {
+//         console.log("SW registration failed: ", registrationError);
+//       });
+//   });
+// }
