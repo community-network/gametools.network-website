8dae6373420889476d9f9d07070da8193ee3c389
Zefanja Jobse <zefanjaj@gmail.com>
2023-10-26 22:27:31 +0200

    lazy load language

--- src/locales/config.ts
+++ src/locales/config.ts
@@ -1,15 +1,10 @@
 import i18n from "i18next";
-import * as translationEN from "./languages/en-US.json";
-import * as translationTR from "./languages/tr-TR.json";
-import * as translationRU from "./languages/ru-RU.json";
-import * as translationCH from "./languages/zh-CN.json";
-import * as translationNL from "./languages/nl-NL.json";
-import * as translationDE from "./languages/de_DE.json";
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 import { format, formatDistanceToNowStrict } from "date-fns";
 import { enUS, tr, zhCN, nl, ru, de } from "date-fns/locale";
 import { registerLocale } from "i18n-iso-countries";
+import resourcesToBackend from "i18next-resources-to-backend";
 
 const locales = {
   "en-US": enUS,
@@ -20,32 +15,11 @@ const locales = {
   "ru-RU": ru,
 };
 
-export const resources = {
-  "en-US": {
-    translation: translationEN,
-  },
-  "tr-TR": {
-    translation: translationTR,
-  },
-  "ru-RU": {
-    translation: translationRU,
-  },
-  "zh-CN": {
-    translation: translationCH,
-  },
-  "de-DE": {
-    translation: translationDE,
-  },
-  "nl-NL": {
-    translation: translationNL,
-  },
-} as const;
-
 i18n
+  .use(resourcesToBackend((language) => import(`./languages/${language}.json`)))
   .use(initReactI18next)
   .use(LanguageDetector)
   .init({
-    resources,
     fallbackLng: "en-US",
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default,
