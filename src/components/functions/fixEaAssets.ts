e815e185a6a1ff3108c92456e2ba5ba382f9f44b
Zefanja Jobse <zefanjaj@gmail.com>
2024-01-24 12:46:23 +0100

    if imgurl is undefined

--- src/components/functions/fixEaAssets.ts
+++ src/components/functions/fixEaAssets.ts
@@ -1,7 +1,10 @@
 export default function sslFix(imgUrl: string | undefined): string | undefined {
+  if (!imgUrl) {
+    return;
+  }
   // also change cdn images to webp variant
   if (imgUrl.includes("cdn.gametools.network")) {
-    return imgUrl?.replace(".png", ".webp").replace(".jpg", ".webp");
+    return imgUrl?.replace(".png", ".webp")?.replace(".jpg", ".webp");
   }
   return imgUrl?.replace(
     "http://eaassets-a.akamaihd.net",
